ae681a3dc024a7d36ce2a2f3c3e9deb6979e0b34
VitaliyG85 <8315588@gmail.com>
2023-11-26 12:52:59 +0500

    First HW - About Me

---
+++ DZ1 (Gibert).py
@@ -0,0 +1,21 @@
+# # print('Привет! Меня зовут Виталий Гиберт, я ученик группы PYTON331 в Академии \"Топ\". \nМоя основная профессия - юрист. Опыт работы в ней - 15 лет. \nЖиву я в г. Екатеринбурге, тоже есть девушка и тоже есть кошка, но кошку зовут не так, как твою :-)\nЕсли говорить про аниме, то предпочтения стандартны и даже банальны - в первую очередь это, конечно, Миядзаки.')
+# mystr = '123' * 3 + '456' * 2 + '789' * 1
+# print(mystr)
+# put your python code here
+
+# def is_palindrome(a: str) -> bool:
+#     if a == a[::-1]:
+#         return True
+#     else:
+#         return False
+#
+#
+# print(is_palindrome('потоп'))
+
+from marvel import full_dict
+from marvel import small_dict
+import typing
+
+small_dict_marvel: str = small_dict
+full_dict_marvel: str = small_dict
+#проверка
